5976c40b8d07f2a17b28da74335a645e3e9161e8
kavindu harshitha <kavindu.harshitha97@gmail.com>
2026-01-17 02:47:07 +0530

    created index file with main exports of the project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { PayHere, generatePaymentHash, verifyPaymentSignature } from "./main";
+export { PayHereError } from "./errors";
+export {
+	PayHereTokenResponse,
+	PayHereTokenErrorResponse,
+	PayHereErrorResponse,
+	PaymentRetrievalResponse,
+	PayHereRefundResponse,
+	Currency,
+} from "./types";
